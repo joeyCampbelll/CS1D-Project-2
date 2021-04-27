113b0dfac13670682f27d2ab7777b4325faa8e42
Jake <jam_merrill@yahoo.com>
2021-04-26 18:14:35 -0700

    merging with correct version of QT

---
+++ build-StadiumTourProject-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,304 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../StadiumTourProject/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[43];
+    char stringdata0[1131];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 22), // "on_tableButton_clicked"
+QT_MOC_LITERAL(2, 34, 0), // ""
+QT_MOC_LITERAL(3, 35, 22), // "on_adminButton_clicked"
+QT_MOC_LITERAL(4, 58, 26), // "on_buildTripButton_clicked"
+QT_MOC_LITERAL(5, 85, 23), // "on_dfsBfsButton_clicked"
+QT_MOC_LITERAL(6, 109, 28), // "on_pushButton_cancel_clicked"
+QT_MOC_LITERAL(7, 138, 28), // "on_viewFiltersButton_clicked"
+QT_MOC_LITERAL(8, 167, 29), // "on_restoreTableButton_clicked"
+QT_MOC_LITERAL(9, 197, 30), // "on_exitTableViewButton_clicked"
+QT_MOC_LITERAL(10, 228, 30), // "on_editSouvenirsButton_clicked"
+QT_MOC_LITERAL(11, 259, 31), // "on_deleteSouvenirButton_clicked"
+QT_MOC_LITERAL(12, 291, 28), // "on_addSouvenirButton_clicked"
+QT_MOC_LITERAL(13, 320, 29), // "on_editSouvenirButton_clicked"
+QT_MOC_LITERAL(14, 350, 36), // "on_selectTeamBox_currentIndex..."
+QT_MOC_LITERAL(15, 387, 4), // "arg1"
+QT_MOC_LITERAL(16, 392, 27), // "on_cancelEditButton_clicked"
+QT_MOC_LITERAL(17, 420, 26), // "on_enterEditButton_clicked"
+QT_MOC_LITERAL(18, 447, 25), // "on_exitEditButton_clicked"
+QT_MOC_LITERAL(19, 473, 26), // "on_cancelAddButton_clicked"
+QT_MOC_LITERAL(20, 500, 25), // "on_enterAddButton_clicked"
+QT_MOC_LITERAL(21, 526, 18), // "on_addButt_clicked"
+QT_MOC_LITERAL(22, 545, 30), // "on_pushButton_cancel_2_clicked"
+QT_MOC_LITERAL(23, 576, 20), // "on_treeView1_clicked"
+QT_MOC_LITERAL(24, 597, 11), // "QModelIndex"
+QT_MOC_LITERAL(25, 609, 5), // "index"
+QT_MOC_LITERAL(26, 615, 20), // "on_fileView1_clicked"
+QT_MOC_LITERAL(27, 636, 23), // "on_FileSelector_clicked"
+QT_MOC_LITERAL(28, 660, 19), // "on_FileView_clicked"
+QT_MOC_LITERAL(29, 680, 34), // "on_pushButton_addDistances_cl..."
+QT_MOC_LITERAL(30, 715, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(31, 739, 27), // "on_pushButton_login_clicked"
+QT_MOC_LITERAL(32, 767, 22), // "on_exit_button_clicked"
+QT_MOC_LITERAL(33, 790, 32), // "on_AddNewDistance_button_clicked"
+QT_MOC_LITERAL(34, 823, 31), // "on_AddNewStadium_button_clicked"
+QT_MOC_LITERAL(35, 855, 31), // "on_RemoveStadium_button_clicked"
+QT_MOC_LITERAL(36, 887, 28), // "on_Stadium_tableView_clicked"
+QT_MOC_LITERAL(37, 916, 30), // "on_editTeamInfo_button_clicked"
+QT_MOC_LITERAL(38, 947, 33), // "on_pushButton_saveChanges_cli..."
+QT_MOC_LITERAL(39, 981, 35), // "on_pushButton_cancelChanges_c..."
+QT_MOC_LITERAL(40, 1017, 35), // "on_pushButton_resetTeamInfo_c..."
+QT_MOC_LITERAL(41, 1053, 37), // "on_pushButton_quickAddStadium..."
+QT_MOC_LITERAL(42, 1091, 39) // "on_pushButton_quickAddDistanc..."
+
+    },
+    "MainWindow\0on_tableButton_clicked\0\0"
+    "on_adminButton_clicked\0"
+    "on_buildTripButton_clicked\0"
+    "on_dfsBfsButton_clicked\0"
+    "on_pushButton_cancel_clicked\0"
+    "on_viewFiltersButton_clicked\0"
+    "on_restoreTableButton_clicked\0"
+    "on_exitTableViewButton_clicked\0"
+    "on_editSouvenirsButton_clicked\0"
+    "on_deleteSouvenirButton_clicked\0"
+    "on_addSouvenirButton_clicked\0"
+    "on_editSouvenirButton_clicked\0"
+    "on_selectTeamBox_currentIndexChanged\0"
+    "arg1\0on_cancelEditButton_clicked\0"
+    "on_enterEditButton_clicked\0"
+    "on_exitEditButton_clicked\0"
+    "on_cancelAddButton_clicked\0"
+    "on_enterAddButton_clicked\0on_addButt_clicked\0"
+    "on_pushButton_cancel_2_clicked\0"
+    "on_treeView1_clicked\0QModelIndex\0index\0"
+    "on_fileView1_clicked\0on_FileSelector_clicked\0"
+    "on_FileView_clicked\0"
+    "on_pushButton_addDistances_clicked\0"
+    "on_pushButton_2_clicked\0"
+    "on_pushButton_login_clicked\0"
+    "on_exit_button_clicked\0"
+    "on_AddNewDistance_button_clicked\0"
+    "on_AddNewStadium_button_clicked\0"
+    "on_RemoveStadium_button_clicked\0"
+    "on_Stadium_tableView_clicked\0"
+    "on_editTeamInfo_button_clicked\0"
+    "on_pushButton_saveChanges_clicked\0"
+    "on_pushButton_cancelChanges_clicked\0"
+    "on_pushButton_resetTeamInfo_clicked\0"
+    "on_pushButton_quickAddStadium_clicked\0"
+    "on_pushButton_quickAddDistances_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      38,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  204,    2, 0x08 /* Private */,
+       3,    0,  205,    2, 0x08 /* Private */,
+       4,    0,  206,    2, 0x08 /* Private */,
+       5,    0,  207,    2, 0x08 /* Private */,
+       6,    0,  208,    2, 0x08 /* Private */,
+       7,    0,  209,    2, 0x08 /* Private */,
+       8,    0,  210,    2, 0x08 /* Private */,
+       9,    0,  211,    2, 0x08 /* Private */,
+      10,    0,  212,    2, 0x08 /* Private */,
+      11,    0,  213,    2, 0x08 /* Private */,
+      12,    0,  214,    2, 0x08 /* Private */,
+      13,    0,  215,    2, 0x08 /* Private */,
+      14,    1,  216,    2, 0x08 /* Private */,
+      16,    0,  219,    2, 0x08 /* Private */,
+      17,    0,  220,    2, 0x08 /* Private */,
+      18,    0,  221,    2, 0x08 /* Private */,
+      19,    0,  222,    2, 0x08 /* Private */,
+      20,    0,  223,    2, 0x08 /* Private */,
+      21,    0,  224,    2, 0x08 /* Private */,
+      22,    0,  225,    2, 0x08 /* Private */,
+      23,    1,  226,    2, 0x08 /* Private */,
+      26,    1,  229,    2, 0x08 /* Private */,
+      27,    1,  232,    2, 0x08 /* Private */,
+      28,    1,  235,    2, 0x08 /* Private */,
+      29,    0,  238,    2, 0x08 /* Private */,
+      30,    0,  239,    2, 0x08 /* Private */,
+      31,    0,  240,    2, 0x08 /* Private */,
+      32,    0,  241,    2, 0x08 /* Private */,
+      33,    0,  242,    2, 0x08 /* Private */,
+      34,    0,  243,    2, 0x08 /* Private */,
+      35,    0,  244,    2, 0x08 /* Private */,
+      36,    1,  245,    2, 0x08 /* Private */,
+      37,    0,  248,    2, 0x08 /* Private */,
+      38,    0,  249,    2, 0x08 /* Private */,
+      39,    0,  250,    2, 0x08 /* Private */,
+      40,    0,  251,    2, 0x08 /* Private */,
+      41,    0,  252,    2, 0x08 /* Private */,
+      42,    0,  253,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   15,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_tableButton_clicked(); break;
+        case 1: _t->on_adminButton_clicked(); break;
+        case 2: _t->on_buildTripButton_clicked(); break;
+        case 3: _t->on_dfsBfsButton_clicked(); break;
+        case 4: _t->on_pushButton_cancel_clicked(); break;
+        case 5: _t->on_viewFiltersButton_clicked(); break;
+        case 6: _t->on_restoreTableButton_clicked(); break;
+        case 7: _t->on_exitTableViewButton_clicked(); break;
+        case 8: _t->on_editSouvenirsButton_clicked(); break;
+        case 9: _t->on_deleteSouvenirButton_clicked(); break;
+        case 10: _t->on_addSouvenirButton_clicked(); break;
+        case 11: _t->on_editSouvenirButton_clicked(); break;
+        case 12: _t->on_selectTeamBox_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 13: _t->on_cancelEditButton_clicked(); break;
+        case 14: _t->on_enterEditButton_clicked(); break;
+        case 15: _t->on_exitEditButton_clicked(); break;
+        case 16: _t->on_cancelAddButton_clicked(); break;
+        case 17: _t->on_enterAddButton_clicked(); break;
+        case 18: _t->on_addButt_clicked(); break;
+        case 19: _t->on_pushButton_cancel_2_clicked(); break;
+        case 20: _t->on_treeView1_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 21: _t->on_fileView1_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 22: _t->on_FileSelector_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 23: _t->on_FileView_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 24: _t->on_pushButton_addDistances_clicked(); break;
+        case 25: _t->on_pushButton_2_clicked(); break;
+        case 26: _t->on_pushButton_login_clicked(); break;
+        case 27: _t->on_exit_button_clicked(); break;
+        case 28: _t->on_AddNewDistance_button_clicked(); break;
+        case 29: _t->on_AddNewStadium_button_clicked(); break;
+        case 30: _t->on_RemoveStadium_button_clicked(); break;
+        case 31: _t->on_Stadium_tableView_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 32: _t->on_editTeamInfo_button_clicked(); break;
+        case 33: _t->on_pushButton_saveChanges_clicked(); break;
+        case 34: _t->on_pushButton_cancelChanges_clicked(); break;
+        case 35: _t->on_pushButton_resetTeamInfo_clicked(); break;
+        case 36: _t->on_pushButton_quickAddStadium_clicked(); break;
+        case 37: _t->on_pushButton_quickAddDistances_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 38)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 38;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 38)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 38;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
