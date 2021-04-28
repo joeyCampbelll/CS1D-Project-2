b3d6b95b2f3ee4b6a3a8fb106ac812e5c443d44a
RyanSolanki <73560558+RyanSolanki@users.noreply.github.com>
2021-04-28 14:19:58 -0700

    Revert "Merge branch 'sac/ryan-solanki' into main"
    This reverts commit 9e1778a7ec17f728a65d91c9babc0d37bd6a930a, reversing
    changes made to 2dfa5873f516981e4370dbc03c607b893aba6a8e.

--- StadiumTourProject/mainwindow.cpp
+++ StadiumTourProject/mainwindow.cpp
@@ -56,9 +56,6 @@ MainWindow::MainWindow(QWidget *parent)
     ui->FileView->setModel(fileModel2);
 
     ui->FileSelector->header()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
-
-    fillStartTeam();
-    initializeList();
 }
 
 MainWindow::~MainWindow()
@@ -100,11 +97,7 @@ void MainWindow::on_adminButton_clicked()
 
 void MainWindow::on_buildTripButton_clicked()
 {
-    ui->label_tripRouteSSR->hide();
-    ui->textBrowser_SSR->hide();
-    ui->pushButton_startTripChooseTeams->hide();
-    ui->tabWidget_tripPlanner->setCurrentIndex(0);
-    ui->stackedWidget->setCurrentIndex(10);
+    //TODO - future build trip code
 }
 
 void MainWindow::on_pushButton_cancel_clicked()
